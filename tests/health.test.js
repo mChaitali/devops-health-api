fccf42be9b5adb6b3e3158a9e009edd8ef7ff82f
Chaitali <chaitali@Amuls-MBP-2.bbrouter>
2026-08-12 18:37:02 +0530

    Updated test import

--- tests/health.test.js
+++ tests/health.test.js
@@ -1,5 +1,5 @@
 const request = require("supertest");
-const { app } = require("../src/app");
+const app = require("../src/app");
 
 describe("GET /health", () => {
   test("returns a healthy response with the required contract", async () => {
